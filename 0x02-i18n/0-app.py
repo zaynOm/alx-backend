dd0b8cfe1d57323ce57dc09fe7ee74c5a6054c3c
zaynom <ouaziz0626@gmail.com>
2024-05-14 18:20:07 +0100

    Task 0: Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""""""
+"""Basic Falsk app"""
 
 from flask import Flask, render_template
 
@@ -8,4 +8,5 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello():
+    """Hello world route"""
     return render_template("0-index.html")
