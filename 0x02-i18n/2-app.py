af51fa149a096e84d4c426f184a95e2e7ca58807
zaynom <ouaziz0626@gmail.com>
2024-05-14 23:41:57 +0100

    Task 2: Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -27,7 +27,7 @@ def get_locale():
 @app.route("/")
 def index():
     """Entry point route"""
-    return render_template("1-index.html")
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
