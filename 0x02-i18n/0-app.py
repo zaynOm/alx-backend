9f3baebf5af2facebf657156d98a86d491ac82c9
zaynom <ouaziz0626@gmail.com>
2024-05-14 18:23:19 +0100

    Task 0: Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,7 +6,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.get("/")
+@app.route("/")
 def hello():
     """Hello world route"""
     return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
