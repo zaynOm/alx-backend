e5f35126413b79594d75fa3fbfa8e102c2f5a28c
zaynom <ouaziz0626@gmail.com>
2024-05-14 19:31:14 +0100

    Task 1: Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Basic Babel setup"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration class"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def hello():
+    """Hello world route"""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
