0e7616d4162e36423fc3606fd4084703bb4030ee
zaynom <ouaziz0626@gmail.com>
2024-06-12 18:44:57 +0100

    Update error message logging

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -7,7 +7,7 @@ const asyncGet = promisify(client.get).bind(client);
 client.on('connect', () => console.log('Redis client connected to the server'));
 
 client.on('error', (err) =>
-  console.log('Redis client not connected to the server: ', err),
+  console.log('Redis client not connected to the server: ', err.message),
 );
 
 function setNewSchool(schoolName, value) {
--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -5,7 +5,7 @@ const client = createClient();
 client.on('connect', () => console.log('Redis client connected to the server'));
 
 client.on('error', (err) =>
-  console.log('Redis client not connected to the server: ', err),
+  console.log('Redis client not connected to the server: ', err.message),
 );
 
 client.hset('HolbertonSchools', 'Portland', 50, redis.print);
