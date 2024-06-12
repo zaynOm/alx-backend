d60c64d8bfd87d0d8214f5e531840fa121fcf1eb
zaynom <ouaziz0626@gmail.com>
2024-06-12 16:50:45 +0100

    Task 4: Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,20 @@
+import redis, { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+client.on('error', (err) =>
+  console.log('Redis client not connected to the server: ', err),
+);
+
+client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+client.hset('HolbertonSchools', 'New York', 20, redis.print);
+client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+
+client.hgetall('HolbertonSchools', (err, reply) => {
+  console.log(err ?? reply);
+});
